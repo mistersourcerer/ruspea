5cbf705d9839fe31720fe7eb4f2374f9e6a37a96
Ricardo Valeriano <mister.sourcerer@gmail.com>
2019-07-25 16:17:27 +0000

    Make the project runnable.

--- spec/ruspea_spec.rb
+++ spec/ruspea_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ruspea do
   it "has a version number" do
     expect(Ruspea::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
