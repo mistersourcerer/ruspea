f7941e2e468b5a4db879fbe47d91c79a841bf6ca
Ricardo Valeriano <mister.sourcerer@gmail.com>
2019-08-19 19:47:38 +0200

    Add the basis for a evaluator.

---
+++ lib/ruspea/evaler/eval.rb
@@ -0,0 +1,40 @@
+module Ruspea::Evaler
+  class Eval
+    include Ruspea::Runtime
+    include Ruspea::Error
+
+    def call(expression, context: Env::Empty.instance)
+      case expression
+      when Numeric
+        expression
+      when String
+        expression
+      when Sym
+        context.lookup expression
+      when List
+        fn = fn_from_invocation(expression, context)
+        arguments = args_from_invocation(expression, context)
+        # TODO: raise if ! respond_to?(:call)
+        fn.call(*arguments, context: context, evaler: self)
+      else
+        raise
+      end
+    end
+
+    private
+
+    def fn_from_invocation(expression, context)
+      context
+        .lookup(expression.head)
+        .tap { |fn|
+          raise Resolution.new(expression.head) if fn.nil?
+        }
+    end
+
+    def args_from_invocation(expression, context)
+      expression
+        .tail
+        .to_a
+    end
+  end
+end
---
+++ spec/ruspea/evaler/eval_spec.rb
@@ -0,0 +1,73 @@
+module Ruspea::Evaler
+  RSpec.describe Eval do
+    subject(:evaler) { described_class.new }
+
+    describe "#call" do
+      it "evaluates symbol to it's value in the context" do
+        number = Ruspea::Runtime::Sym.new("number")
+        cxt = Ruspea::Runtime::Env.new.tap { |env| env.define number, 420 }
+
+        result = evaler.call number, context: cxt
+        expect(result).to eq 420
+      end
+
+      it "evaluates Numerics to themselves" do
+        expect(evaler.call(420)).to eq 420
+      end
+
+      it "evaluates Strings to themselves" do
+        expect(evaler.call("hello world")).to eq "hello world"
+      end
+
+      context "invocations" do
+        it "treats Lists as function invocation" do
+          fn_name = Ruspea::Runtime::Sym.new("fn")
+          fn = Ruspea::Runtime::Lm.new body: [420]
+          cxt = Ruspea::Runtime::Env.new.tap { |env| env.define fn_name, fn }
+
+          list = Ruspea::Runtime::List.create fn_name
+
+          result = evaler.call list, context: cxt
+          expect(result).to eq 420
+        end
+
+        it "raises if no function with the given name is found" do
+          list = Ruspea::Runtime::List.create Ruspea::Runtime::Sym.new("no")
+
+          expect {
+            evaler.call(list)
+          }.to raise_error Ruspea::Error::Resolution
+        end
+
+        it "raises if list.head ! respond_to? :call"
+
+        it "evaluates the parameters before calling the function" do
+          # (def number 420)
+          # (defn fn [number_arg] number_arg)
+          # (fn number) => 420
+          # (fn 13) => 13
+
+          fn_name = Ruspea::Runtime::Sym.new("fn")
+          number = Ruspea::Runtime::Sym.new("number")
+          fn = Ruspea::Runtime::Lm.new(
+            params: [Ruspea::Runtime::Sym.new("number_arg")],
+            body: [Ruspea::Runtime::Sym.new("number_arg")],
+          )
+          ctx = Ruspea::Runtime::Env.new.tap { |env|
+            env.define fn_name, fn
+            env.define number, 420
+          }
+
+          list = Ruspea::Runtime::List.create(
+            fn_name,
+            Ruspea::Runtime::Sym.new("number")
+          )
+          list_with_13 = Ruspea::Runtime::List.create(fn_name, 13)
+
+          expect(evaler.call(list, context: ctx)).to eq 420
+          expect(evaler.call(list_with_13, context: ctx)).to eq 13
+        end
+      end
+    end
+  end
+end
