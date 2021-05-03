48094243d0c7008efe0a7beb25428fbe1c195a7c
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2021-05-03 18:03:22 -0300

    Add some coverage measuring devices.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ require "bundler/setup"
 Bundler.require :default
 require "ruspea"
 
+begin
+  require "simplecov"
+  SimpleCov.start
+rescue LoadError
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
