a999296b54dcfe4d27363b62ca0cee5301b50b79
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2021-05-16 00:35:59 -0300

    Add (Cdr)

---
+++ spec/ruspea/lisp/cdr_spec.rb
@@ -0,0 +1,37 @@
+module Ruspea
+  RSpec.describe Lisp::Cdr do
+    include Core::Casting
+
+    let(:ctx) { {} }
+    subject(:cdr) { described_class.new }
+
+    describe "#call" do
+      it "raises if parameter is not a list" do
+        expect { cdr.call(1, ctx) }.to raise_error(
+          Error::Execution,
+          /Argument should be a list, received 1 instead/
+        )
+      end
+
+      it "raises if wrong arity is used" do
+        expect { cdr.call(List(), ctx) }.to raise_error(
+          Error::Execution,
+          /Argument should be a list/
+        )
+
+        expect { cdr.call(List(List(1), 1), ctx) }.to raise_error(
+          Error::Execution,
+          /Wrong number of arguments: given 2, expected 1/
+        )
+      end
+
+      it "returns the rest of a list" do
+        expect(cdr.call(List(List(1, 2)), ctx)).to eq List(2)
+      end
+
+      it "returns nil if the list empty" do
+        expect(cdr.call(List(List()), ctx)).to eq List()
+      end
+    end
+  end
+end
